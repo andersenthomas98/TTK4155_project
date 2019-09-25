77fc2941e07689f7b8d2b0a94a5e66f8da58e028
andersenthomas98 <47141397+andersenthomas98@users.noreply.github.com>
2019-09-25 18:34:09 +0200

    Lab fire med pong

--- lab4/lab4/Drivers/menu.c
+++ lab4/lab4/Drivers/menu.c
@@ -72,69 +72,6 @@ void MENU_navigate(menu_ptr item) {
 	}
 }
 
-/*
-void MENU_init(void) {
-	menu_t mainMenu;
-	menu_ptr mainMenuPtr = &mainMenu;
-	mainMenu.name = "Main menu";
-	mainMenu.num_children = 3;
-	mainMenu.selectedChild = 0;
-
-	menu_t sub_menu0;
-	menu_ptr sub_menu0Ptr = &sub_menu0;
-	sub_menu0.parent = mainMenuPtr;
-	sub_menu0.num_children = 0;
-	sub_menu0.name = "sub0   ";
-
-	menu_t sub_menu1;
-	menu_ptr sub_menu1Ptr = &sub_menu1;
-	sub_menu1.parent = mainMenuPtr;
-	sub_menu1.num_children = 3;
-	mainMenu.selectedChild = 0;
-	sub_menu1.name = "sub1   ";
-
-	menu_t sub_menu2;
-	menu_ptr sub_menu2Ptr = &sub_menu2;
-	sub_menu2.parent = mainMenuPtr;
-	sub_menu2.num_children = 0;
-	sub_menu2.name = "sub2   ";
-
-
-	menu_t sub_menu00;
-	menu_ptr sub_menu00Ptr = &sub_menu00;
-	sub_menu00.parent = sub_menu1Ptr;
-	sub_menu00.num_children = 0;
-	sub_menu00.name = "sub00   ";
-
-	menu_t sub_menu10;
-	menu_ptr sub_menu10Ptr = &sub_menu10;
-	sub_menu10.parent = sub_menu1Ptr;
-	sub_menu10.num_children = 0;
-	sub_menu10.name = "sub10   ";
-
-	menu_t sub_menu20;
-	menu_ptr sub_menu20Ptr = &sub_menu20;
-	sub_menu20.parent = sub_menu1Ptr;
-	sub_menu20.num_children = 0;
-	sub_menu20.name = "sub20   ";
-
-
-	sub_menu1.children[0] = sub_menu00Ptr;
-	sub_menu1.children[1] = sub_menu10Ptr;
-	sub_menu1.children[2] = sub_menu20Ptr;
-
-
-	mainMenu.parent = NULL;
-	mainMenu.children[0] = sub_menu0Ptr;
-	mainMenu.children[1] = sub_menu1Ptr;
-	mainMenu.children[2] = sub_menu2Ptr;
-
-	
-	
-	MENU_print_children(mainMenuPtr);
-}
-*/
-
 void MENU_veryFunInvertingFunction(){
 	OLED_clearAll();
 	OLED_refresh();
@@ -217,60 +154,9 @@ void MENU_pong() {
 				OLED_set_pixel((int)leftPaddleX, (int)leftPaddleY + i);
 				OLED_set_pixel((int)rightPaddleX, (int)rightPaddleY + i);
 			}
-			OLED_set_pixel((int)x, (int)y);
+			OLED_set_pixel((int)x, (itn)y);
 			OLED_refresh();
 			_delay_ms(2);
 		}
 	}
 }
-
-/*
-void MENU_pongX4() {
-	int x = 255;
-	float y = 127;
-	int x_vel = 2;
-	int y_vel = 0;
-	int rightPaddleY = 124;
-	int leftPaddleY = 124;
-	int leftPaddleX = 12;
-	int rightPaddleX = 496;
-	int rightPoints = 0;
-	int leftPoints = 0;
-	while (1) {
-		float x = 255;
-		float y = 127;
-		x_vel = 2;
-		y_vel = 0;
-		while (1){
-			rightPaddleY = ADC_slider_right();
-			leftPaddleY = ADC_slider_left();
-			x += x_vel;
-			y += y_vel;
-			if (x <= 0) {
-				leftPoints += 1;
-				break;
-			}
-			else if (x >= 508) {
-				rightPoints += 1;
-				break;
-			}
-			else if (x > leftPaddleX-2 && x < leftPaddleX + 2 && x < leftPaddleY + 16 && x > leftPaddleY - 16){
-				
-			}
-			else if (x > rightPaddleX-2 && x < rightPaddleX + 2 && x < leftPaddleY + 16 && x > leftPaddleY - 16){
-
-			}
-			if (y < 0 || y >= 63) {
-				y_vel = y * abs(y_vel)/abs(y);
-			}
-			else if (y == 0){
-				y_vel = -y_vel;
-			}
-			OLED_clearAll();
-			OLED_set_pixel(x/4, y/4);
-			OLED_refresh();
-			_delay_ms(100);
-		}
-	}
-}
-*/
